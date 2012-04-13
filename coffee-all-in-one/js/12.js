e34a490fd844375c649eadd45e8cb0b59ef72295
Takazudo <takazudo@gmail.com>
2012-04-14 05:38:31 +0900

    added coffee-all-in-one

---
+++ coffee-all-in-one/js/12.js
@@ -0,0 +1,52 @@
+(function() {
+  var str;
+
+  str = 'sweet';
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  str = 'tasty';
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+  (function() {
+    return alert("coffee is " + str);
+  })();
+
+}).call(this);
